b154a978b7ace770eb83bb9233a39dc356eb7db3
abbygklui <63834576+abbygklui@users.noreply.github.com>
2026-02-26 16:44:31 -0500

    Update home screen with WeeklyTrackerCard and Iconify icon
    Added WeeklyTrackerCard widget to the home screen and replaced settings icon with Iconify version.

--- sunscreen_app/lib/screens/home/home_screen.dart
+++ sunscreen_app/lib/screens/home/home_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
+import 'package:iconify_flutter/icons/ph.dart';
 import '../../core/theme/app_colors.dart';
-import '../../core/theme/app_text_styles.dart';
 import '../settings/settings_screen.dart';
 import 'widgets/uv_card.dart';
 import 'widgets/timer_ring.dart';
 import 'widgets/apply_button.dart';
+import 'widgets/weekly_tracker_card.dart';
 
 /// Main home screen showing UV info and reapplication timer.
 class HomeScreen extends ConsumerWidget {
@@ -18,8 +20,11 @@ class HomeScreen extends ConsumerWidget {
         title: const Text('Sunscreen'),
         actions: [
           IconButton(
-            icon: const Icon(Icons.settings_rounded),
-            color: AppColors.warmGray,
+            icon: const Iconify(
+              Ph.gear_bold,
+              color: AppColors.warmGray,
+              size: 24,
+            ),
             onPressed: () {
               Navigator.push(
                 context,
@@ -37,6 +42,8 @@ class HomeScreen extends ConsumerWidget {
           child: Column(
             children: [
               UvCard(),
+              SizedBox(height: 16),
+              WeeklyTrackerCard(),
               SizedBox(height: 24),
               TimerRing(),
               SizedBox(height: 32),
